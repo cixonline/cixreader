1b0cb523d0d78ac33752286f42bfa74dedb4e3e8
cixonline <steve@icuk.net>
2015-11-27 19:09:02 +0000

    Update build version to 1103.

--- CIXReader/Properties/AssemblyInfo.cs
+++ CIXReader/Properties/AssemblyInfo.cs
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.61.1102")]
-[assembly: AssemblyFileVersion("1.61.1102")]
+[assembly: AssemblyVersion("1.61.1103")]
+[assembly: AssemblyFileVersion("1.61.1103")]
