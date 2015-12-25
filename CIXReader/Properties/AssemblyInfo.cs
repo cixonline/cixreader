6ed503abf9fa514f7a7210a168b5586a754af435
cixonline <steve@icuk.net>
2015-12-25 09:15:03 +0000

    Prepare for next build

--- CIXReader/Properties/AssemblyInfo.cs
+++ CIXReader/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 
 // Version information for this assembly. The values are dynamically replaced
 // when running the build script.
-[assembly: AssemblyVersion("1.61.1113")]
-[assembly: AssemblyFileVersion("1.61.1113")]
+[assembly: AssemblyVersion("1.61.1114")]
+[assembly: AssemblyFileVersion("1.61.1114")]
