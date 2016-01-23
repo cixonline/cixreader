15f181a4915ae764c0c576e36014bd0a004c6858
cixonline <steve@icuk.net>
2016-01-23 09:16:46 +0000

    Add User Agent when making web request for images

--- CIXClient/Collections/ImageRequestorTask.cs
+++ CIXClient/Collections/ImageRequestorTask.cs
@@ -94,6 +94,7 @@ namespace CIXClient.Collections
 
                     HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(realImageURL);
                     webRequest.AllowWriteStreamBuffering = true;
+                    webRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)";
 
                     using (WebResponse _WebResponse = webRequest.GetResponse())
                     {
