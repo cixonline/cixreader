654084785064166c3c672da0fff5c04b5d03edc0
cixonline <steve@icuk.net>
2016-02-10 13:20:04 +0000

    Bump up version

--- CIXMarkup/Properties/AssemblyInfo.cs
+++ CIXMarkup/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 
 // Version information for this assembly. The values are dynamically replaced
 // when running the build script.
-[assembly: AssemblyVersion("1.61.1118")]
-[assembly: AssemblyFileVersion("1.61.1118")]
+[assembly: AssemblyVersion("1.62.1119")]
+[assembly: AssemblyFileVersion("1.62.1119")]
