9326c941a43c7111ac46725fb6de0a7e065c882e
cixonline <steve@icuk.net>
2016-03-01 10:43:14 +0000

    Update build numbers and use https for app links

--- CIXReader/Properties/AssemblyInfo.cs
+++ CIXReader/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 
 // Version information for this assembly. The values are dynamically replaced
 // when running the build script.
-[assembly: AssemblyVersion("1.62.1119")]
-[assembly: AssemblyFileVersion("1.62.1119")]
+[assembly: AssemblyVersion("1.62.1120")]
+[assembly: AssemblyFileVersion("1.62.1120")]
